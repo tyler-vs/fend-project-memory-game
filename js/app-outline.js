f02f7640ac4ee9703313bfb693494ed427f0b001
tyler-vs <tylervucf@knights.ucf.edu>
2018-08-18 15:51:02 -0400

    add: new doc to outline app classes

---
+++ js/app-outline.js
@@ -0,0 +1,40 @@
+// app-outline.js
+//
+// possitble objects/classes?
+//
+// - app
+// - stopwatch/timer
+// - star rating
+//
+
+
+// app
+// ---
+
+var MemoryGameApp = (function() {
+
+'use strict';
+
+//
+// variables
+//
+
+
+
+
+//
+// private functions
+//
+
+
+//
+// public functions
+//
+
+
+//
+// inits/event listeners
+//
+
+})();
+
